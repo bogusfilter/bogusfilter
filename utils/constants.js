b688e555099e459cbf8252205a99f22f3829ce1b
Rob Sawyer <rsawyer@tripwire.com>
2018-03-31 21:09:07 -0700

    Constants update

--- utils/constants.js
+++ utils/constants.js
@@ -2,15 +2,6 @@
  * Constants that are reused throughout the app.
  * utils/constants.js
  */
-try {
-  const isProduction = process.env && process.env.NODE_ENV ? true : false;
-  if (isProduction) {
-    const dotenv = require('dotenv').load();
-  }
-} catch (err) {
-  console.error(err);
-  return false;
-}
 const apiKey = process.env.BOGUS_FILTER_API_KEY;
 const endpointSSL = 'https://api.bogusfilter.com/v1';
 const endpoint = 'http://api.bogusfilter.com/v1';
@@ -23,7 +14,6 @@ if (!apiKey) {
 }
 
 module.exports = {
-  isProduction,
   apiKey,
   endpoint,
   endpointSSL,

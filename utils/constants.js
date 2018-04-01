f0f119ed0d19578cc5f927110649739f2440d64d
Rob Sawyer <rsawyer@tripwire.com>
2018-03-31 19:31:17 -0700

    Updates related to fs bug

--- utils/constants.js
+++ utils/constants.js
@@ -2,7 +2,7 @@
  * Constants that are reused throughout the app.
  * utils/constants.js
  */
-const isProduction = process.env.NODE_ENV || 'local';
+const isProduction = window.process.env.NODE_ENV || 'local';
 const apiKey = window.process.env.BOGUS_FILTER_API_KEY;
 const endpointSSL = 'https://api.bogusfilter.com/v1';
 const endpoint = 'http://api.bogusfilter.com/v1';

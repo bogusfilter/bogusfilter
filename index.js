cbcf8bab2f13ac215c963c272a434d07e9cb2646
Rob Sawyer <rsawyer@tripwire.com>
2018-03-31 20:45:49 -0700

    Environment variable updates.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@
  * Documentation
  * @see https://docs.bogusfilter.com
  */
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 var Promise = require('bluebird');
 var request = require('./utils/request');
 var constants = require('./utils/constants');
--- utils/constants.js
+++ utils/constants.js
@@ -2,12 +2,11 @@
  * Constants that are reused throughout the app.
  * utils/constants.js
  */
-if (!process.env) {
-  console.error('Unable to react process.env.');
-  return false;
-}
 const isProduction = process.env.NODE_ENV || 'local';
-const apiKey = process.env.BOGUS_FILTER_API_KEY || console.error('Unable to load BOGUS_FILTER_API_KEY environment variable.');
+if (!isProduction) {
+  const dotenv = require('dotenv').load();
+}
+const apiKey = process.env.BOGUS_FILTER_API_KEY || console.error('BOGUS_FILTER_API_KEY environment variable NOT found.');
 const endpointSSL = 'https://api.bogusfilter.com/v1';
 const endpoint = 'http://api.bogusfilter.com/v1';
 

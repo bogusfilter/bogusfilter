b13a63309e9183b1767138090ef7cecfa3a79cb4
Rob Sawyer <rsawyer@tripwire.com>
2018-04-02 01:22:55 -0700

    Removed bogus.json reference

--- utils/constants.js
+++ utils/constants.js
@@ -7,32 +7,32 @@ const endpointSSL = 'https://api.bogusfilter.com/v1';
 const endpoint = 'http://api.bogusfilter.com/v1';
 
 // Try to load the bogus.json file with the key.
-if (!apiKey) {
-  const fs = require('file-system');
-  const path = require('path');
-  let bogusFile;
-  try {
-    bogusFile = path.resolve(process.cwd(), 'bogus.json');
-  } catch(err) {
-    console.error(err);
-  }
-  if (bogusFile) {
-    let fileContent;
-    try {
-      fileContent = fs.readFileSync(bogusFile);
-    } catch (err) {
-      console.error(err);
-    }
-    let parsedJSON;
-    try {
-      parsedJSON = JSON.parse(fileContent);
-      apiKey = parsedJSON.key;
-      process.env.BOGUS_FILTER_API_KEY = apiKey;
-    } catch (err) {
-      console.error(err);
-    }
-  }
-}
+// if (!apiKey) {
+//   const fs = require('file-system');
+//   const path = require('path');
+//   let bogusFile;
+//   try {
+//     bogusFile = path.resolve(process.cwd(), 'bogus.json');
+//   } catch(err) {
+//     console.error(err);
+//   }
+//   if (bogusFile) {
+//     let fileContent;
+//     try {
+//       fileContent = fs.readFileSync(bogusFile);
+//     } catch (err) {
+//       console.error(err);
+//     }
+//     let parsedJSON;
+//     try {
+//       parsedJSON = JSON.parse(fileContent);
+//       apiKey = parsedJSON.key;
+//       process.env.BOGUS_FILTER_API_KEY = apiKey;
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   }
+// }
 
 if (!apiKey) {
   console.error('\n---------------------------------------------------------------------------------------');
